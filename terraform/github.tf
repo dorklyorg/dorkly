59d7e1b871e8b39be7ca1894323680e818cd0878
drichelson <drichelson@users.noreply.github.com>
2024-05-13 16:54:38 +0900

    terraform: upload yaml files

--- terraform/github.tf
+++ terraform/github.tf
@@ -1,6 +1,6 @@
 provider "github" {
-#   token = "" Alternately specified using GITHUB_TOKEN env var
-   owner = "dorklyorg"
+  #   token = "" Alternately specified using GITHUB_TOKEN env var
+  owner = "dorklyorg"
 }
 
 resource "github_repository" "dorkly_repo" {
@@ -28,30 +28,24 @@ resource "github_actions_secret" "aws_secret_key_secret" {
   plaintext_value = aws_iam_access_key.dorkly_write_user_access_key.secret
 }
 
-resource "github_repository_file" "dorkly_workflows" {
+# Upload each file to the repo using the same relative path minus 'files/'
+resource "github_repository_file" "dorkly_flags_project" {
+  for_each = toset(
+    [
+      "files/project/project.yml",
+      "files/project/flags/boolean1.yml",
+      "files/project/flags/rollout1.yml",
+      "files/project/environments/production/boolean1.yml",
+      "files/project/environments/staging/boolean1.yml",
+      "files/project/environments/production/rollout1.yml",
+      "files/project/environments/staging/rollout1.yml",
+    ])
   repository          = github_repository.dorkly_repo.name
   branch              = "main"
-  file                = ".github/workflows/dorkly.yml"
-  content             = file("files/dorkly.yml")
+  file                = trimprefix(each.key, "files/")
+  content             = file(each.key)
   commit_message      = "Managed by Terraform"
   overwrite_on_create = true
 }
 
-resource "github_repository_file" "dorkly_example_flags" {
-  repository          = github_repository.dorkly_repo.name
-  branch              = "main"
-  file                = "flags/test-data.json"
-  content             = file("files/flags/test-data.json")
-  commit_message      = "Managed by Terraform"
-  overwrite_on_create = true
-}
-
-resource "github_repository_file" "dorkly_example_env" {
-  repository          = github_repository.dorkly_repo.name
-  branch              = "main"
-  file                = "flags/test.json"
-  content             = file("files/flags/test.json")
-  commit_message      = "Managed by Terraform"
-  overwrite_on_create = true
-}
 
